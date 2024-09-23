f7c3f683d289a590e6a4fcf8909bca2162db0aa1
File Tibor József <filetibor2002@gmail.com>
2024-09-23 16:26:36 +0200

    fix: move micro-orm initialization to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import "./globals.css";
+import { MikroORM } from "@mikro-orm/postgresql";
+import config from "@/mikro-orm.config";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const orm = await MikroORM.init(config);
+  console.log(orm.em);
+  console.log(orm.schema);
   return (
     <html lang="en">
       <body>
--- src/instrumentation.ts
+++
@@ -1,8 +0,0 @@
-import { MikroORM } from '@mikro-orm/postgresql';
-import config from './mikro-orm.config';
-
-export async function register() {
-    const orm = await MikroORM.init(config);
-    console.log(orm.em);
-    console.log(orm.schema);
-}
