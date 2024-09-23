3d2eb4d5e34ba2a646b2475c51e36e181d472e05
Bagoly Luca Enikő <bagolyluca03@gmail.com>
2024-09-23 16:55:24 +0200

    feat: Added RootLayout and edited page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
+import "./globals.css";
+
 export default function Home() {
-  return (
-    <p>Test</p>
-  );
+    return (
+        <div>
+            <h2>Üdvözöljük a Lakatos Brendonék Menhelyének weboldalán!</h2>
+            <p>Örömmel látjuk, hogy ellátogatott hozzánk!</p>
+            <p>A Lakatos Brendonék Menhelyének célja, hogy biztonságos és szeretetteljes
+                környezetet biztosítson a rászoruló kutyák számára. Minden kutya egyedi történettel és személyiséggel
+                rendelkezik, és mindegyikük megérdemli a második esélyt.</p>
+
+            <p>Fedezze fel kutyáinkat, akik várják, hogy új otthonra leljenek! Böngésszen a profilok között, és ha
+                megtalálja az ideális társát, ne habozzon kapcsolatba lépni velünk. Önnek is lehetősége van segíteni:
+                támogathat minket önkéntes munkával vagy adományokkal.</p>
+
+            <p>Köszönjük, hogy hozzájárul a kutyák boldogságához és jólétéhez!</p>
+        </div>
+    );
 }
