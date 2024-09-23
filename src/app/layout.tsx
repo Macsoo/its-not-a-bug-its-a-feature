fc124538320a5e03f3e8f6701063d13057c4918a
Bagoly Luca Enikő <bagolyluca03@gmail.com>
2024-09-23 17:16:34 +0200

    feat: edit layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,12 +12,33 @@ export default async function RootLayout({
   console.log(orm.em);
   console.log(orm.schema);
   return (
-    <html lang="en">
+      <html lang="hu">
+      <head>
+          <link href="https://fonts.googleapis.com/css2?family=Itim&display=swap" rel="stylesheet"/>
+          <title> Lakatos Brendonék Menhelye</title>
+      </head>
       <body>
-        <div className="content">
-            {children}
+      <div className="header">
+          <img src="" alt="logo"/>
+        <h1>Lakatos Brendonék Menhelye</h1>
+        <div>
+          <button>Regisztráció</button>
+          <button>Belépés</button>
         </div>
+      </div>
+      <div className="menu">
+        <div>
+          <button>Kutyáink...</button>
+          <button>Rólunk</button>
+        </div>
+        <button>Fiókom</button>
+      </div>
+      <div className="content">
+        <div className="card">
+          {children}
+        </div>
+      </div>
       </body>
-    </html>
+      </html>
   );
 }
