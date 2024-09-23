449fae8a9ef3855908c6b93c3cba831df9134df2
Bagoly Luca Enikő <bagolyluca03@gmail.com>
2024-09-23 20:58:12 +0200

    feat: added pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,41 @@
 import "./globals.css";
+import Link from 'next/link';
 
 export default async function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                             children,
+                                         }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-      <html lang="hu">
-      <head>
-          <link href="https://fonts.googleapis.com/css2?family=Itim&display=swap" rel="stylesheet"/>
-          <title> Lakatos Brendonék Menhelye</title>
-      </head>
-      <body>
-      <div className="header">
-          <img src="" alt="logo"/>
-        <h1>Lakatos Brendonék Menhelye</h1>
-        <div>
-          <button>Regisztráció</button>
-          <button>Belépés</button>
+
+
+    return (
+        <html lang="hu">
+        <head>
+            <link href="https://fonts.googleapis.com/css2?family=Itim&display=swap" rel="stylesheet"/>
+            <title> Lakatos Brendonék Menhelye</title>
+        </head>
+        <body>
+        <div className="header">
+            <img src="" alt="logo"/>
+            <Link href="/"><h1 className="title">Lakatos Brendonék Menhelye</h1></Link>
+            <div>
+                <Link href="/register"><button id="register">Regisztráció</button></Link>
+                <Link href="/login"><button id="login">Belépés</button></Link>
+            </div>
         </div>
-      </div>
-      <div className="menu">
-        <div>
-          <button>Kutyáink...</button>
-          <button>Rólunk</button>
+        <div className="menu">
+            <div>
+                <Link href="/dogs"><button id="dogs">Kutyáink...</button></Link>
+                <Link href="/about"><button id="about">Rólunk</button></Link>
+            </div>
+            <Link href="/account"><button id="account">Fiókom</button></Link>
         </div>
-        <button>Fiókom</button>
-      </div>
-      <div className="content">
-        <div className="card">
-          {children}
+        <div className="content">
+            <div className="card">
+                {children}
+            </div>
         </div>
-      </div>
-      </body>
-      </html>
-  );
+        </body>
+        </html>
+    );
 }
---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+import "../globals.css";
+export default function Login() {
+    return (
+        <p>Ide jön a belépési felület</p>
+    );
+}
