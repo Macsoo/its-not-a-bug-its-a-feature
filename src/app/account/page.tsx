ac27786be75804b7860228052ca631ab1268ca0a
Bagoly Luca Enikő <bagolyluca03@gmail.com>
2024-09-25 14:07:41 +0200

    fix: fixed layout

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,11 +1,15 @@
 'use client';
 import "../globals.css";
+import RequestCard from "@/components/requestCard";
+import {currentRole} from "@/components/roles";
+
 export default function AccountPage() {
     return (
         <div className="content">
             <div className="card">
-                <p>Ide jönnek a fiók adatai</p>
+                <h2>Jogosultság: {currentRole}</h2>
             </div>
+            <RequestCard key={0}/>
         </div>
     );
 }
