6584173539f25986117aef7ab43bf714d718c758
Kicskeb <banvolgyi.bence@gmail.com>
2024-09-28 15:52:05 +0200

    feat: implemented pictureRepository.ts

--- src/server/pictureRepository.ts
+++ src/server/pictureRepository.ts
@@ -1,25 +1,77 @@
 'use server';
 
-export async function add() {
+import {DogImage, PrismaClient} from "@prisma/client";
 
+export async function addPicture(picture: DogImage, isPrimary: boolean) {
+    const prisma = new PrismaClient();
+    prisma.$transaction(async (trx) => {
+        const img = await trx.dogImage.create({
+            data: picture,
+        })
+        if (isPrimary) {
+            if (picture.dogId === null) {
+                throw Error("Given picture has no dogId");
+            }
+            trx.dog.update({
+                data: {
+                    primaryImgId: img.id
+                },
+                where: {
+                    id: picture.dogId
+                }
+            })
+        }
+    })
 }
 
-export async function get() {
-
+export async function getPicture(pictureId: number) {
+    const prisma = new PrismaClient();
+    return prisma.$transaction(async (trx) => {
+        return trx.dogImage.findFirst({
+            where: {
+                id: pictureId
+            }
+        })
+    })
 }
 
-export async function list() {
-
+export async function listAllPictures() {
+    const prisma = new PrismaClient();
+    return prisma.$transaction(async (trx) => {
+        return trx.dog.findMany();
+    })
 }
 
-export async function deletePicture() {
-
+export async function deletePicture(pictureId: number) {
+    const prisma = new PrismaClient();
+    prisma.$transaction(async (trx) => {
+        trx.dogImage.delete({
+            where: {
+                id: pictureId
+            }
+        })
+    })
 }
 
-export async function update() {
-
+export async function updatePicture(picture: { id: number, dogId?: number, path?: string }) {
+    const prisma = new PrismaClient();
+    prisma.$transaction(async (trx) => {
+        trx.dogImage.update({
+            data: picture,
+            where: {
+                id: picture.id
+            }
+        })
+    })
 }
 
-export async function listDogPictures() {
-
+export async function listDogPictures(dogId: number) {
+    const prisma = new PrismaClient();
+    return prisma.$transaction(async (trx) => {
+        return trx.dogImage.findMany({
+            where: {
+                dogId: dogId
+            }
+        });
+    })
 }
