78959e298a5a1c72eba2b3f8240ed013553115a7
Kicskeb <banvolgyi.bence@gmail.com>
2024-10-21 15:34:04 +0200

    feat: reworked adding and updating dog profiles,

--- src/app/add_dog/page.tsx
+++ src/app/add_dog/page.tsx
@@ -19,6 +19,22 @@ export default function AddDog() {
     const [description, setDescription] = useState("");
     const [breed, setBreed] = useState("");
 
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleValidation = (e: React.FormEvent<HTMLInputElement>) => {
+        const input = e.currentTarget;
+        if (!input.validity.valid) {
+            if (input.validity.patternMismatch) {
+                setErrorMessage('Please enter exactly 15 digits for the chip number.');
+            } else {
+                setErrorMessage('Invalid input.');
+            }
+        } else {
+            setErrorMessage('');
+        }
+    };
+
+
     return (
         <div className="content">
             <div className={`card`}>
@@ -46,65 +62,78 @@ export default function AddDog() {
                             value={chipId}
                             pattern={"\\d{15}"}
                             onChange={(e) => setChipId(e.target.value)}
-                        /> </div>
-                        <div className={`form`}>
-                            <label htmlFor="name" className={`pr-1 w-[48px]`}>Név:</label>
-                            <input
-                                className={`dogUpdateInput`}
-                                id="name"
-                                type="text"
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
-                            />
-                        </div>
-                        <div className={`form`}>
-                            <label htmlFor="age" className={`pr-1 w-[48px]`}>Kor:</label>
-                            <input
-                                className={`dogUpdateInput`}
-                                id="age"
-                                type="number"
-                                value={age}
-                                onChange={(e) => setAge(parseInt(e.target.value))}
-                            />
-                        </div>
-                        <div className={`form`}>
-                            <label htmlFor="gender" className={`pr-1 w-[48px]`}>Nem:</label>
-                            <select
-                                className={`dogUpdateInput`}
-                                id="gender"
-                                value={gender}
-                                onChange={(e) => {
-                                    if (e.target.value === 'Male' || e.target.value === 'Female') {
-                                        setGender(e.target.value)
-                                    }
-                                }}
-                            >
-                                <option value="Male">Hím</option>
-                                <option value="Female">Nőstény</option>
-                            </select>
-                        </div>
-                        <div className={`form`}>
-                            <label htmlFor="breed" className={`pr-1 w-[48px]`}>Fajta:</label>
-                            <input
-                                className={`dogUpdateInput`}
-                                id="breed"
-                                type="text"
-                                value={breed}
-                                onChange={(e) => setBreed(e.target.value)}
-                            />
-                        </div>
-                            <div className={`flex flex-row items-center p-2`}>
-                                <label htmlFor="description" className={`pr-1 w-[48px]`}>Leírás:</label>
-                                <textarea
-                                    className={`dogUpdateInput w-full h-[150px]`}
-                                    id="description"
-                                    value={description}
-                                    onChange={(e) => setDescription(e.target.value)}
-                                />
-                            </div>
-                            <div className={`flex flex-row items-center justify-center`}>
-                                <button id={`updateDog`} type="submit">Kutya Hozzáadása</button>
-                            </div>
+                            autoFocus={true}
+                            onInvalid={handleValidation}
+                            onInput={handleValidation}
+                            required
+                        />
+                    </div>
+
+                    {errorMessage && <span style={{ color: 'red' }}>{errorMessage}</span>}
+
+                    <div className={`form`}>
+                        <label htmlFor="name" className={`pr-1 w-[48px]`}>Név:</label>
+                        <input
+                            className={`dogUpdateInput`}
+                            id="name"
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className={`form`}>
+                        <label htmlFor="age" className={`pr-1 w-[48px]`}>Kor:</label>
+                        <input
+                            className={`dogUpdateInput`}
+                            id="age"
+                            type="number"
+                            value={age}
+                            onChange={(e) => setAge(parseInt(e.target.value))}
+                            required
+                        />
+                    </div>
+                    <div className={`form`}>
+                        <label htmlFor="gender" className={`pr-1 w-[48px]`}>Nem:</label>
+                        <select
+                            className={`dogUpdateInput`}
+                            id="gender"
+                            value={gender}
+                            onChange={(e) => {
+                                if (e.target.value === 'Male' || e.target.value === 'Female') {
+                                    setGender(e.target.value)
+                                }
+                            }}
+                        >
+                            <option value="Male">Hím</option>
+                            <option value="Female">Nőstény</option>
+                        </select>
+                    </div>
+                    <div className={`form`}>
+                        <label htmlFor="breed" className={`pr-1 w-[48px]`}>Fajta:</label>
+                        <input
+                            className={`dogUpdateInput`}
+                            id="breed"
+                            type="text"
+                            value={breed}
+                            onChange={(e) => setBreed(e.target.value)}
+                        />
+                    </div>
+                    <div className={`flex flex-row items-center p-2`}>
+                        <label htmlFor="description" className={`pr-1 w-[48px]`}>Leírás:</label>
+                        <textarea
+                            className={`dogUpdateInput w-full h-[150px]`}
+                            id="description"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                        />
+                    </div>
+                    <div>
+
+                    </div>
+                    <div className={`flex flex-row items-center justify-center`}>
+                        <button id={`updateDog`} type="submit">Kutya Hozzáadása</button>
+                    </div>
                 </form>
             </div>
         </div>
--- src/app/dogs/dog_update/[dog_id]/page.tsx
+++ src/app/dogs/dog_update/[dog_id]/page.tsx
@@ -1,13 +1,15 @@
 'use client';
-import { useRouter } from 'next/navigation';
-import React, { useState } from 'react';
+import {useRouter} from 'next/navigation';
+import React, {useEffect, useState} from 'react';
 import {Dog} from "@prisma/client";
 import {useServerAction} from "@/utils";
 import {getDog, updateDog} from "@/server/dogRepository";
+import {useDropzone} from 'react-dropzone'
 
-export default function UpdateDog({ params }: { params: { dog_id: string } }) {
+export default function UpdateDog({params}: { params: { dog_id: string } }) {
     const router = useRouter();
 
+    const [loading, setLoading] = useState(true);
     const dogId = parseInt(params.dog_id, 10);
     const [dog, setDog] = useState<Dog | null>(null);
     const [name, setName] = useState(dog?.name);
@@ -20,11 +22,8 @@ export default function UpdateDog({ params }: { params: { dog_id: string } }) {
         setAge(dog?.age);
         setGender(dog?.gender);
         setDescription(dog?.description);
+        setLoading(false);
     });
-
-    if (!dog) {
-        return <div>Kutya nem található!</div>;
-    }
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
@@ -38,62 +37,108 @@ export default function UpdateDog({ params }: { params: { dog_id: string } }) {
         router.push(`/dogs/${dogId}`);
     };
 
-    return (
-        <div className={`content`}>
-            <div className={`card`}>
-                <h2>{dog.name}  adatainak szerkesztése</h2>
-                <form onSubmit={handleSubmit}>
-                    <div className={`form`}>
-                        <label htmlFor="name" className={`pr-1 w-[48px]`}>Név:</label>
-                        <input
-                            className={`dogUpdateInput`}
-                            id="name"
-                            type="text"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                        />
-                    </div>
-                    <div className={`form`}>
-                        <label htmlFor="age" className={`pr-1 w-[48px]`}>Kor:</label>
-                        <input
-                            className={`dogUpdateInput`}
-                            id="age"
-                            type="number"
-                            value={age}
-                            onChange={(e) => setAge(parseInt(e.target.value))}
-                        />
-                    </div>
-                    <div className={`form`}>
-                        <label htmlFor="gender" className={`pr-1 w-[48px]`}>Nem:</label>
-                        <select
-                            className={`dogUpdateInput`}
-                            id="gender"
-                            value={gender}
-                            onChange={(e) => {
-                                if (e.target.value === 'Male' || e.target.value === 'Female') {
-                                    setGender(e.target.value)
-                                }
-                            }}
-                        >
-                            <option value="male">Hím</option>
-                            <option value="female">Nőstény</option>
-                        </select>
-                    </div>
-                    <div className={`flex flex-row items-center p-2`}>
-                        <label htmlFor="description" className={`pr-1 w-[48px]`}>Leírás:</label>
-                        <textarea
-                            className={`dogUpdateInput w-full h-[150px]`}
-                            id="description"
-                            value={description}
-                            onChange={(e) => setDescription(e.target.value)}
-                        />
-                    </div>
-                    <div className={`flex flex-row items-center justify-center`}>
+    const [files, setFiles] = useState<File[]>([]);
+    const {getRootProps, getInputProps} = useDropzone({
+        accept: {
+            'image/*': []
+        },
+        onDrop: acceptedFiles => {
+            setFiles(acceptedFiles.map(file => Object.assign(file, {
+                preview: URL.createObjectURL(file)
+            })));
+        }
+    });
 
-                    <button id={`updateDog`} type="submit">Frissítés</button>
-                    </div>
-                </form>
+    const thumbs = files.map(file => (
+        <div key={file.name}>
+            <div>
+                <img
+                    src={file.webkitRelativePath}
+                    onLoad={() => {
+                        URL.revokeObjectURL(file.webkitRelativePath)
+                    }}
+                />
             </div>
         </div>
-    );
-}
+    ));
+
+    useEffect(() => {
+        return () => files.forEach(file => URL.revokeObjectURL(file.webkitRelativePath));
+    }, [files]);
+
+    return <>
+        {dog &&
+            (
+                <div className={`content`}>
+                    <div className={`card`}>
+                        <h2>{dog.name} adatainak szerkesztése</h2>
+                        <form onSubmit={handleSubmit}>
+                            <div className={`form`}>
+                                <label htmlFor="name" className={`pr-1 w-[48px]`}>Név:</label>
+                                <input
+                                    className={`dogUpdateInput`}
+                                    id="name"
+                                    type="text"
+                                    onChange={(e) => setName(e.target.value)}
+                                    defaultValue={name}
+                                />
+                            </div>
+                            <div className={`form`}>
+                                <label htmlFor="age" className={`pr-1 w-[48px]`}>Kor:</label>
+                                <input
+                                    className={`dogUpdateInput`}
+                                    id="age"
+                                    type="number"
+                                    value={age}
+                                    onChange={(e) => setAge(parseInt(e.target.value))}
+                                    defaultValue={age}
+                                />
+                            </div>
+                            <div className={`form`}>
+                                <label htmlFor="gender" className={`pr-1 w-[48px]`}>Nem:</label>
+                                <select
+                                    className={`dogUpdateInput`}
+                                    id="gender"
+                                    value={gender}
+                                    onChange={(e) => {
+                                        if (e.target.value === 'Male' || e.target.value === 'Female') {
+                                            setGender(e.target.value)
+                                        }
+                                    }}
+                                >
+                                    <option value="male">Hím</option>
+                                    <option value="female">Nőstény</option>
+                                </select>
+                            </div>
+                            <div className={`flex flex-row items-center p-2`}>
+                                <label htmlFor="description" className={`pr-1 w-[48px]`}>Leírás:</label>
+                                <textarea
+                                    className={`dogUpdateInput w-full h-[150px]`}
+                                    id="description"
+                                    value={description}
+                                    onChange={(e) => setDescription(e.target.value)}
+                                    defaultValue={description}
+                                />
+                            </div>
+                            <section className="container">
+                                <div {...getRootProps({className: 'dropzone'})}>
+                                    <input {...getInputProps()} />
+                                    <p>Drag `&#39;`n drop some files here, or click to select files</p>
+                                </div>
+                                <aside>
+                                    {thumbs}
+                                </aside>
+                            </section>
+                            <div className={`flex flex-row items-center justify-center`}>
+
+                                <button id={`updateDog`} type="submit">Frissítés</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            )
+        }
+        {!dog && !loading && <p>A keresett kutya nem található!</p>}
+        {!dog && loading && <p>Kérjük várjon, a kutyus adatai épp töltődnek...</p>}
+    </>
+};
