d1ce60962b25141affc1c33697c47819e8517bf1
Bagoly Luca Enikő <bagolyluca03@gmail.com>
2024-12-06 11:02:51 +0100

    feat: added filter frontend

--- src/components/dogList.tsx
+++ src/components/dogList.tsx
@@ -22,6 +22,36 @@ export default function DogList() {
     });
     return (
         <div className={`content`}>
+            <div className={`w-full`}>
+                <form id={"filter"}>
+                    {
+                        //TODO: show only available choices
+                    }
+                    <div>
+                        <label>Kor:</label>
+                        <select id={"age"}>
+                            <option value={"null"}></option>
+                            <option value={'TODO'}>TODO</option>
+                        </select>
+
+                    </div>
+                    <div>
+                        <label>Fajta:</label>
+                        <select id={"breed"}>
+                            <option value={"null"}></option>
+                            <option value={'TODO'}>TODO</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label>Nem:</label>
+                        <select id={"gender"}>
+                            <option value={"null"}></option>
+                            <option value={'TODO'}>TODO</option>
+                        </select>
+                    </div>
+                    <input id={`search`} type="submit" value="Keresés"/>
+                </form>
+            </div>
             {dogsWithImages.length > 0 && dogsWithImages.map((dog) => (
                 <div className={`w-full`} key={dog.dog.id}>
                     <DogCard
